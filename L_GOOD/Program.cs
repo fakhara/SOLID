f6259f791ef8e5a3458f47f3e480b3918df6e7c2
Fakhara <fakhara.imran82@gmail.com>
2024-10-15 11:24:17 +0200

    Change in L_GOOD  mai file.

--- L_GOOD/Program.cs
+++ L_GOOD/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-           
+            IForm rekangle = new Rektangel
+            {
+                Bredd = 5,
+                Höjd = 10
+            };
+            Console.WriteLine($"Rektangel Area: {rekangle.Area()}");
+
+            IForm kvadrat = new Kvadrat
+            {
+                Sida = 5
+            };
+            Console.WriteLine($"Kvadrat Area: {kvadrat.Area()}");
+
+            IForm triangel = new Triangel
+            {
+                Base = 5,
+                Height = 10,
+            };
+            Console.WriteLine($"Triangel Area: {triangel.Area()}");
+
         }
     }
 }
