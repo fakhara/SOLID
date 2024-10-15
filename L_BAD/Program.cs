5220e57b5e9c69093dc820e21a78c1288ec40185
Fakhara <fakhara.imran82@gmail.com>
2024-10-15 11:12:48 +0200

    Change in L_BAD and L_Good

--- L_BAD/Program.cs
+++ L_BAD/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Rektangel rektangel = new Rektangel
+            {
+                Bredd = 5,
+                Höjd = 10
+            };
+            Console.WriteLine($"Rektangel Area: {rektangel.Area()}");
+            Rektangel kvadratSomRektangel = new Kvadrat();
+            kvadratSomRektangel.Bredd = 5;
+            kvadratSomRektangel.Höjd = 10;
+
+            Console.WriteLine($"Kvadrat Som Rektangel Arae: {kvadratSomRektangel.Area()}");
         }
     }
 }
--- L_GOOD/Program.cs
+++ L_GOOD/Program.cs
@@ -4,15 +4,7 @@
     {
         static void Main(string[] args)
         {
-            IForm rektangel = new Rektangel { Bredd = 10, Höjd= 5};
-            IForm triangel = new Triangel { Base = 6, Height = 8 };
-            IForm kvadrat = new Kvadrat { Sida = 4 };
-            var calculator = new AreaCalculator();
-            Console.WriteLine($"Area of Rectangle: {calculator.CalculateArea(rektangel)}");
-            Console.WriteLine($"Area of Triangle: {calculator.CalculateArea(triangel)}");
-            Console.WriteLine($"Area of Kvadrat: {calculator.CalculateArea(kvadrat)}");
-
-            Console.ReadLine();
+           
         }
     }
 }
